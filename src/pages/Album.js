7315c00c6c1e3c32316cd688f061fda7ecd7c278
Emerson Marinho <Emerson_marinhoaz@hotmail.com>
2023-03-14 16:14:18 -0300

    feat: requisito 07

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import musicsAPI from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    musics: [],
+    albumName: '',
+    artistName: '',
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const musics = await musicsAPI(id);
+
+    this.setState((prevState) => ({
+      musics: [...prevState.musics, ...musics.slice(1)],
+      albumName: musics[0].collectionName,
+      artistName: musics[0].artistName,
+    }));
+  }
+
+  renderMusicList = (musics) => musics.map((music, index) => (
+    <li key={ `${music.trackId}-${index}` }>
+      <p>{music.trackName}</p>
+      <audio controls data-testid="audio-component">
+        <source src={ music.previewUrl } type="audio/mp3" />
+        <track kind="captions" srcLang="en" label="English captions" />
+      </audio>
+    </li>
+  ));
+
   render() {
+    const { musics, albumName, artistName } = this.state;
+
     return (
-      <div data-testid="page-album" />
+      <div>
+        <h2 data-testid="artist-name">{artistName}</h2>
+        <h3 data-testid="album-name">{albumName}</h3>
+        <ul>{this.renderMusicList(musics)}</ul>
+      </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
